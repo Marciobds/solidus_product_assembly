a7e1c6d559616fab2977dd65e3b86214d7ef6e34
Márcio Bortolini <marciobds@live.it>
2021-03-15 11:56:12 -0300

    Comṕatibilidade + traduções + ajustes + última versão solidus

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
     resources :products do
+      resources :assemblies_parts, only: [] do
+        collection do
+          post :update_positions
+        end
+      end
+      
       resources :parts do
         member do
           post :select
@@ -11,6 +17,7 @@ Spree::Core::Engine.routes.draw do
         end
         collection do
           post :available
+          post :update_positions
           get  :selected
         end
       end
